140ae53cf8b11c2a10682c3d6c530a554aeceaf5
LaiseMendes <73499584+LaiseMendes@users.noreply.github.com>
2021-09-29 18:05:45 -0300

    Criação calculadora de gastos

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace Repositorio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Do you know how much water, food and waste you produce or consume? Let's find out!\nType how much WATER you use on a daily basis:");        
+        
+            double water = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type how much food you consume:");
+
+            double food = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type how much waste you produce:");
+
+            double waste = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Great! Now, for how many people you want to calculate for?");
+
+            double people = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{people} people, use {people*water}L water, consume {people*food}Kg food and produce {people*waste}Kg waste.");
+
+
         }
     }
 }
